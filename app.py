26238cced41ffb6ac33b8e75b73523d7cf1252ad
Arij Thabet <arijthabet01@gmail.com>
2025-10-05 17:52:30 +0100

    added ci pipeline

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
+import logging
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
+app.logger.setLevel(logging.INFO)  
 @app.route('/')
 def home():
     app.logger.info("Page d'accueil visitée")
